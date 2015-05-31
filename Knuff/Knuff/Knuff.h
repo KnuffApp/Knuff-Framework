e262ad4e54dbd7977ae9b7af2bf0a465456af6c0
Simon Blommegård <simon@blommegard.se>
2015-05-31 13:58:17 +0200

    Adding framework.

---
+++ Knuff/Knuff/Knuff.h
@@ -0,0 +1,19 @@
+//
+//  Knuff.h
+//  Knuff
+//
+//  Created by Simon Blommegard on 31/05/15.
+//  Copyright (c) 2015 Bowtie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Knuff.
+FOUNDATION_EXPORT double KnuffVersionNumber;
+
+//! Project version string for Knuff.
+FOUNDATION_EXPORT const unsigned char KnuffVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Knuff/PublicHeader.h>
+
+
